a773ca3e7722927832a6a4d888b2fe4b3e842592
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-03-22 10:14:17 -0400

    army list

---
+++ vue/src/array.js
@@ -0,0 +1,59 @@
+const character = [
+  {
+    uuid: {
+      name: 'Agent Venom',
+      threat: 4,
+      faction: ['Guardians of the Galaxy, SHIELD, Web Warriors'],
+      dataSheetFront:
+        'https://s3.us-west-2.amazonaws.com/assets.acrossthebifrost.com/100/01/P2P_Stat_CP100_AgentVenom_healthy.png',
+      dataSheetBack:
+        'https://s3.us-west-2.amazonaws.com/assets.acrossthebifrost.com/100/01/P2P_Stat_CP100_AgentVenom_Injured.png'
+    }
+  },
+  {
+    uuid: {
+      name: 'Amazing Spiderman',
+      threat: 5,
+      faction: ['Defenders', 'Web Warriors'],
+      dataSheetFront:
+        'http://cloud-3.steamusercontent.com/ugc/1706288949317713904/9D7F79A0F5F6C37707854327C723BF988512305E/',
+      dataSheetBack:
+        'http://cloud-3.steamusercontent.com/ugc/1706288949317714027/8BF995491B2F4B38D4E27A940D21772C7B027B47/'
+    }
+  },
+  {
+    uuid: {
+      name: 'Doctor Strange',
+      threat: 5,
+      faction: ['Convocation', 'Defenders', 'Midnight Sons'],
+      dataSheetFront:
+        'https://s3-us-west-2.amazonaws.com/assets.acrossthebifrost.com/023/1/dr_strange_healthy.jpg',
+      dataSheetBack:
+        'https://s3-us-west-2.amazonaws.com/assets.acrossthebifrost.com/023/1/dr_strange_injured.jpg'
+    }
+  },
+  {
+    uuid: {
+      name: 'Iron Man',
+      threat: 3,
+      faction: ['Avengers', 'SHIELD'],
+      dataSheetFront:
+        'https://s3.us-west-2.amazonaws.com/assets.acrossthebifrost.com/001/1/iron_man_healthy_revised.png',
+      dataSheetBack:
+        'https://s3.us-west-2.amazonaws.com/assets.acrossthebifrost.com/001/1/iron_man_injured_revised.png'
+    }
+  },
+  {
+    uuid: {
+      name: 'Magneto',
+      threat: 6,
+      faction: ['Brotherhood', 'Cabal'],
+      dataSheetFront:
+        'https://s3-us-west-2.amazonaws.com/assets.acrossthebifrost.com/042/1/magneto_healthy.jpg',
+      dataSheetBack:
+        'https://s3-us-west-2.amazonaws.com/assets.acrossthebifrost.com/042/1/magneto_injured.jpg'
+    }
+  }
+]
+
+export { character }
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,21 +1,28 @@
-<template>
-  <ul>
-    <li v-for="model in models" :key="model">{{ model }}</li>
-  </ul>
-</template>
-
 <script>
+import { character } from '../array'
 export default {
   name: 'HomeView',
   components: {},
   data() {
     return {
-      student: 'Harry',
-      graduated: false,
-      models: ['Stormsurge', 'Knight Galant', 'Stompa']
+      models: character
+    }
+  },
+  methods: {
+    test() {
+      console.log(this.models)
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.main {
+}
+</style>
+
+<template>
+  <div class="main">
+    <h1 v-for="model in models" :key="model">{{ model }}</h1>
+  </div>
+</template>
