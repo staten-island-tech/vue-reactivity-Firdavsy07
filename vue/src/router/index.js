18de260c3c4f6fc9dc1aab9290dfb506ae605466
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-04-04 09:43:03 -0400

    Update index.js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -11,7 +11,8 @@ const router = createRouter({
     },
     {
       path: '/about',
-      name: 'about'
+      name: 'about',
+      component: HomeView
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
